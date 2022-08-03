72c719d52235583bd82050a53022c9a079d8732e
master-irfan <irfan.ip0909@gmail.com>
2022-08-03 20:24:30 +0530

    updated in uat

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-
+line is updating in uat
 Hello i am updating this line from master branch
-updating this line from dev branch 
+updating this line from dev branchthis 
 updating in dev. branch ..
 :This files contains the deployments code
